81f29f79b622ce13ec464e88a9f9c4a574bf11a4
lxy <2305195328@11.com>
2023-02-26 09:03:35 +0800

    写点人工智能知识

---
+++ src/2-3-13.py
@@ -0,0 +1,6 @@
+year = int(input('输入年份:'))
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print("True")
+else:
+    print("False")
---
+++ src/2-3-14.py
@@ -0,0 +1,6 @@
+number_1 = int(input('输入最大边界值'))
+for i in range(1, number_1):
+    if i % 0 == 0:
+        pass
+    else:
+        print(i)
